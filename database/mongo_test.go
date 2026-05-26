b9cdb0c25d098583a09b79dd20d45dda932fbcd4
joao.pereira <joao.pereira@ebinterchange.com.br>
2026-05-26 15:32:57 -0300

    ajuste remoção da uri

--- database/mongo_test.go
+++ database/mongo_test.go
@@ -5,7 +5,7 @@ import (
 )
 
 func TestGetPokemon(t *testing.T) {
-	Connect("mongodb+srv://admin:macacoverde@botdiscord.o7x8gjt.mongodb.net/")
+
 	tests := []struct {
 		// description of this test case
 		// Named input parameters for target function.
